df20c9ff833ae92360316baeb4f95e024e38f346
dangthemen10 <phanhaidangkgumt@gmail.com>
2021-02-01 23:32:45 +0700

    Btap3t

--- src/main/java/BaiTap/BaiTap3.java
+++ src/main/java/BaiTap/BaiTap3.java
@@ -1,55 +1,15 @@
 package BaiTap;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;
-import java.io.Serializable;
-import java.util.List;
-import java.util.ArrayList;
-import java.io.FileOutputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.ObjectInputStream;
+import java.lang.String;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class BaiTap3 {
     private static final String filepath = "Student.txt";
-    static ObjectOutputStream os;
-    static ObjectInputStream is;
-
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Student a = new Student(1, "apple", 21, "male");
-        readFile();
-
-    }
-
-//        public static void writeToFile(List<Object> fruits) throws IOException {
-//            os = new ObjectOutputStream(new FileOutputStream(file));
-//            os.writeObject(fruits);
-//            os.close();
-//
-//        }
-
-    public static void readFile() throws ClassNotFoundException, IOException {
-        is = new ObjectInputStream(new FileInputStream(filepath));
-        List<Object> input = (List<Object>) is.readObject();
-        List<Object> checkList = new ArrayList<>();
-        // this will contain the list of the objects
-        for (Object l : input) {
-            checkList.add(l.getClass().getSimpleName());
-            if (l instanceof Student) {
-                Student app = (Student) l;
-                System.out.println(app.id);
-                System.out.println(app.name);
-                System.out.println(app.age);
-                System.out.println(app.gender);
-            }
-        }
-        System.out.println(checkList);
-
-        is.close();
-    }
-
     public static class Student implements Serializable{
         private static final long serialVersionUID = 1L;
         private int id;
@@ -57,47 +17,54 @@ public class BaiTap3 {
         private int age;
         private String gender;
 
-        public Student(int id, String apple, int age, String male) {
-
-        }
-        public Student(int id, String name, byte age,
-                       String gender) {
-            super();
+        public Student() {
             this.id = id;
             this.name = name;
             this.age = age;
             this.gender = gender;
         }
-        public int getId() {
+
+        public Student(String s, String s1) {
+        }
+
+        public int getId(){
             return id;
         }
-
-        public void setId(int id) {
+        public void setId(int id){
             this.id = id;
         }
-
-        public String getName() {
+        public String getName(){
             return name;
         }
-
-        public void setName(String name) {
+        public void setName(String name){
             this.name = name;
         }
-
-        public int getAge() {
+        public int getAge(){
             return age;
         }
-
-        public void setAge(byte age) {
+        public void setAge(int age){
             this.age = age;
         }
-
-        public String getGender() {
+        public String getGender(){
             return gender;
         }
-
-        public void setGender(String gender) {
+        public void setGender(String gender){
             this.gender = gender;
         }
+    }
+
+    public static void main(String args[]){
+        final ArrayList<Student> studentList = new ArrayList<>();
+        try {
+            Stream<String> lines = Files.lines(Paths.get(filepath));
+            lines.forEach((String t) -> {
+                String[] parse = t.split(":");
+                if(parse.length<2) return;
+                studentList.add(new Student(parse[0], parse[1]));
+            });
+        } catch (IOException ex) {
+            System.out.println("Unable to open student file." + ex.toString());
+        }
+        System.out.println(studentList);
     }
 }
